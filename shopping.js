ebf92e06ce06bf4d44d7d8a60a355f29fa478030
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2020-04-27 05:47:45 +0100

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,58 @@
+
+var C1 = {
+    id: "C1",
+    name: "Crystal",
+    description: "A hand-dug Crystal from the Ozarks",
+    price: 20
+
+};
+
+
+
+
+var cart = Array();
+var totalCost = 0;
+var productList =""
+localStorage.setItem('cart', existing.toString());
+
+
+function getTotal() {
+    var cart = (localStorage.getItem("cart"));
+    cart = JSON.parse(cart);
+    total = 0;
+    response = "";
+    for (i = 0; i < cart.length; i++) {
+        total = total + cart[i].price;
+        response = response + cart[i].name + " : $" + cart[i].price + "\n"
+    }
+
+    response = response + "Your total is $" + total;
+    alert(response);
+    document.getElementById("insert").innerHTML = response;
+   // document.getElementById(response).value = response;
+
+   /* for (i = 0; i < cart.length; i++) {
+        alert("These are your items: \n" + cart[i].name + " : " + cart[i].price);
+    }*/
+
+}
+
+function addToCartC1() {
+    var cart = (localStorage.getItem("cart"));
+    cart = JSON.parse(cart);
+    C1 = new Object();
+    C1.name = "Crystal"
+    C1.price = 20;
+    cart.push(C1);
+    items = cart.length;
+    alert(C1.name + " has been added to your cart. You now have a total of " + items + " items in your cart");
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+function emptyCart() {
+    var cart = (localStorage.getItem("cart"));
+    cart = cart ? cart.split(',') : [];
+    cart = [];
+    alert("All items removed from cart.");
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
